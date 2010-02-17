1f12f60e9b17f08f746d1498c06eaed6d2d258d1
Richard Sentino <richard.sentino@gmail.com>
2010-02-17 09:42:39 +0800

    Added new CouchDB Document commands     - all_docs     - all_docs_by_seq     - execute a View of a particular Document Design zafu.sh     - new switch option for temporary env variables .zafu     - new env variable

--- zafu/zafu.sh
+++ zafu/zafu.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #******************************************************************************
-# 		   FILE:	zafu.sh 
-#	DESCRIPTION:	This script utility is part of the zafu project.
-# 		VERSION:	version 0.1.0 
+#          FILE:  zafu.sh 
+#   DESCRIPTION:  This script utility is part of the zafu project.
+#       VERSION:  version 0.1.0 
 # 	
-# 			Copyright (C) 2010 Richard Sentino (richard.sentino@gmail.com)
+# Copyright (C) 2010 Richard Sentino (richard.sentino@gmail.com)
 #******************************************************************************
 
 CMD_STATUS=0
 ZAFU_CONFIG="/usr/local/etc/zafu/.zafu"
+ZAFU_SCRIPT=/usr/local/etc/zafu/zafu.sh
 
 # Show help menu
 usage()
@@ -22,7 +23,7 @@ OPTIONS:
    -c	Update zafu config values
    -n	Config name
    -v   Config value
-   -r   Resource a TCL script
+   -t	Set temp config values
    -v   verbose mode
 
 Copyright (C) 2010 Richard Sentino (richard.sentino@gmail.com)
@@ -36,16 +37,28 @@ set_config() {
 	mv $TMP_FILE $ZAFU_CONFIG	
 }
 
+set_tmp_config() {
+	if [ "$TMP_DB_NAME" != "default_zafu_db_name" ]; then
+		`$ZAFU_SCRIPT -c -n TMP_COUCHDB_DATABASE -v $TMP_DB_NAME`
+	fi
+}
+
 main() {
 	if [ "$SET_CONFIG" == "true" ]; then
 		set_config
 	fi	
+	
+	# setup a temporary env variable on the fly
+	if [ "$SET_TMP_CONFIG" == "true" ]; then
+		set_tmp_config
+	fi	
+	
 }
 
 #******************************************************************************
-#									MAIN                                           
+#                               MAIN                                           
 #******************************************************************************
-while getopts "hrcn:v:" OPTION; do
+while getopts "hrctn:v:d:" OPTION; do
 	case $OPTION in
 		c)
 			SET_CONFIG=true			
@@ -55,6 +68,12 @@ while getopts "hrcn:v:" OPTION; do
 			;;
 		v)
 			CVALUE=$OPTARG
+			;;
+		t)
+			SET_TMP_CONFIG=true
+			;;
+		d)
+			TMP_DB_NAME=$OPTARG
 			;;
 		h)
 			echo "help"
